e24f4e9e08fd8ab1eefa028c70f9b935da6b074f
Nguyen-Tan-Xanh <nguyentanxanh0507@gmail.com>
2024-08-04 14:12:13 +0700

    Update gRPC Server and Client

---
+++ src/gRPC.Client/Program.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Net.Client;
+using gRPC.Server;
+using System;
+
+namespace gRPC.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var grpcChannel = GrpcChannel.ForAddress("https://localhost:5001");
+            var greeterClient = new Greeter.GreeterClient(grpcChannel);
+            var greeterReply = greeterClient.SayHello(new HelloRequest
+            {
+                Name = "Nguyen Tan Xanh",
+            });
+            Console.WriteLine(greeterReply.ToString());
+
+            var provinceClient = new ProvinceRPC.ProvinceRPCClient(grpcChannel);
+            var provinceDetail = provinceClient.GetProvinceDetail(new GetProvinceRequest
+            {
+                Id = "01",
+            });
+            Console.WriteLine(provinceDetail.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/gRPC.Server/Services/ProvinceService.cs
@@ -0,0 +1,32 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace gRPC.Server.Services
+{
+    public class ProvinceService : ProvinceRPC.ProvinceRPCBase
+    {
+        private readonly ILogger<ProvinceService> _logger;
+        public ProvinceService(ILogger<ProvinceService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<GetProvinceReply> GetProvinceDetail(GetProvinceRequest request, ServerCallContext context)
+        {
+            var districts = new List<District>()
+            {
+                new District {Id = "1", Name = "Ben Luc", Level = "Huyen"},
+                new District {Id = "2", Name = "Tan An", Level = "Thanh Pho"},
+            };
+            return Task.FromResult(new GetProvinceReply
+            {
+                Id = request.Id,
+                Name = "Long An",
+                Level = "Tinh",
+                Districts = { districts }
+            });
+        }
+    }
+}
